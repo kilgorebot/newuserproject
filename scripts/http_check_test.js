dc74e75aafd495fe44336235b8f0d7e6bb314502
LeeKilgore <ronaldleemankilgore@gmail.com>
2017-01-13 13:02:59 -0700

    Update http_check_test.js

--- scripts/http_check_test.js
+++ scripts/http_check_test.js
@@ -9,9 +9,40 @@
 //
 // Commands:
 //  
-
+/*
+(function() {
+  module.exports = function(robot) {
+    var docsUrl, pagePrefix, referenceLink, sqlSuffix;
+    docsUrl = 'http://www.w3schools.com/sql/';
+    sqlSuffix = '.asp';
+    pagePrefix = 'sql_';
+    referenceLink = function(res) {
+      var keywordName, ref, response;
+      keywordName = (ref = res.match[1]) != null ? ref.replace(' ', '') : void 0;
+      response = "Check out " + docsUrl + pagePrefix;
+      response += keywordName;
+      response += sqlSuffix;
+      return res.send(response);
+    };
+    return robot.hear(/^!sql (\w*)/i, function(res) {
+      return referenceLink(res);
+    });
+  };
+}).call(this);
+*/
 module.exports = function(robot) {
-  robot.hear(/^!jquery (.*)/i, function(msg){
+
+  robot.hear(/^!jquery (\w*)/i, function(msg){
+    var docsUrl = 'http://www.w3schools.com/sql/';
+    var pageSuffix = '.asp';
+    var pagePrefix = 'sql_';
+    var referenceLink;
+    referenceLink = function(responseUrl) {
+      var keywordName;
+      var ref;
+      var response;
+      
+    }
     // build the first url to check
     var helpLink = "http://kilgorei.pairserver.com/";
 	  var http=require('http');
