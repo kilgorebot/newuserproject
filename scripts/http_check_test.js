ed233e2e804a10cdf091189bb274652a69865998
LeeKilgore <ronaldleemankilgore@gmail.com>
2017-01-12 21:35:15 -0700

    Update http_check_test.js

--- scripts/http_check_test.js
+++ scripts/http_check_test.js
@@ -13,7 +13,7 @@
 module.exports = function(robot) {
   robot.hear(/^!jquery (.*)/i, function(msg){
 	var http=require('http');
-	var request =http.get("http://kilgorei.pairserver.com/", function(response){
+	var request =http.get("http://kilgorei.pairserver.com/fadggf", function(response){
 	      msg.reply(response.statusCode);
 	  });
 
