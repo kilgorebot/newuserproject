b8c4cf6fc35f984eb443b8d8559fde17465cb84c
iankilgore <ianmacduffkilgore@gmail.com>
2017-01-12 21:20:08 -0700

    edited response jquery

--- scripts/http_check_test.js
+++ scripts/http_check_test.js
@@ -12,6 +12,6 @@
 
 module.exports = function(robot) {
     robot.hear(/^!jquery (.*)/i, function(msg){
-        msg.reply("YES");
+        msg.reply("YES I'M WORKING LEAVE THE BUSY ROBOT ALONE!!!");
     });
 }
