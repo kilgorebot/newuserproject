62c62b4fafa79b0422514b63238a5f1c0661d569
iankilgore <ianmacduffkilgore@gmail.com>
2017-01-14 18:34:06 -0700

    reverting 2 commits back

--- scripts/http_check_test.js
+++ scripts/http_check_test.js
@@ -46,17 +46,14 @@ module.exports = function(robot) {
       responseUrl += keywordName;
       responseUrl += pageSuffix;
       var http=require('http');
-	    var helpRequested;
-      helpRequested = function(responseUrl) {
-        http.get(responseUrl, function(response){
-	        if (response.statusCode == 200) {
-            return responseUrl;
-          } else {
-            return docsUrl;
-          }
-        });
-      }
-      return helpRequested;
+	    var helpRequested =http.get(responseUrl, function(response){
+	      if (response.statusCode == 200) {
+          return responseUrl;
+        } else {
+          return docsUrl;
+        }
+      });
+      return responseUrl;
     }
 
     return msg.send(referenceLink(msg));
